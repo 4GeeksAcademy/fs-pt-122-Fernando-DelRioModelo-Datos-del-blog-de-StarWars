a2d8056199afa4a9d8e3ec6b53cee8d0f0464a9b
Fernando Del Rio <fernandogondelrio@gmail.com>
2026-01-21 18:30:49 +0000

    feat: añadir modelos de usuarios, personajes, planetas y favoritos

--- src/models.py
+++ src/models.py
@@ -10,10 +10,57 @@ class User(db.Model):
     password: Mapped[str] = mapped_column(nullable=False)
     is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
 
+    favorites = db.relationship("Favorite", back_populates="user")
+
 
     def serialize(self):
         return {
             "id": self.id,
             "email": self.email,
             # do not serialize the password, its a security breach
+        }
+    
+class Character(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(100), nullable=False)
+
+    favorites = db.relationship("Favorite", back_populates="character")
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+        }
+
+
+class Planet(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(100), nullable=False)
+
+    favorites = db.relationship("Favorite", back_populates="planet")
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+        }
+
+
+class Favorite(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+
+    user_id: Mapped[int] = mapped_column(db.ForeignKey("user.id"), nullable=False)
+    character_id: Mapped[int | None] = mapped_column(db.ForeignKey("character.id"), nullable=True)
+    planet_id: Mapped[int | None] = mapped_column(db.ForeignKey("planet.id"), nullable=True)
+
+    user = db.relationship("User", back_populates="favorites")
+    character = db.relationship("Character", back_populates="favorites")
+    planet = db.relationship("Planet", back_populates="favorites")
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "user_id": self.user_id,
+            "character_id": self.character_id,
+            "planet_id": self.planet_id,
         }
